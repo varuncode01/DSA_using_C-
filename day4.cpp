39e8cbc311f07ba4cd8f1c8fe3e94711e81a89fa
varuncode01 <varun1992.xyz@gmail.com>
2025-07-19 16:24:00 +0530

    Commit4_1

---
+++ day4.cpp
@@ -0,0 +1,33 @@
+//
+// Created by hp on 19-07-2025.
+//
+#include <iostream>
+using namespace std;
+
+void square_pattern(int n) {
+    int num = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+}
+
+void star_square_pattern(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n = 5;
+    square_pattern(n);
+    cout << endl;
+    star_square_pattern(n);
+    return 0;
+}
