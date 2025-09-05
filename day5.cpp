b9817aaedb025bf98ffb91b6961a516daba0d87d
varuncode01 <varun1992.xyz@gmail.com>
2025-09-05 22:22:26 +0530

    Decimal to Binary converaion

--- day5.cpp
+++ day5.cpp
@@ -1,3 +1,28 @@
 // Binary Number System
 #include <iostream>
 using namespace std;
+
+int dectobinary(int decnum){
+    int ans = 0, pow = 1;
+
+    while (decnum > 0)
+    {
+        int rem = decnum % 2;
+        decnum = decnum / 2;
+        
+        ans = ans + (rem * pow);
+        pow = pow * 10;
+    }
+    return ans;    
+}
+
+int main() {
+    int n;
+    cout << "Enter a decimal number: ";
+    cin >> n;
+
+    int binarynum = dectobinary(n);
+    cout << "Binary of " << n << " is " << binarynum << endl;
+
+    return 0;
+}
