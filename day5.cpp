443990cda9a81d50c9987048ca46eacfbd2799a6
varuncode01 <varun1992.xyz@gmail.com>
2025-09-05 23:03:16 +0530

    binary to decimal

--- day5.cpp
+++ day5.cpp
@@ -17,18 +17,41 @@ int dectobinary(int decnum){
     return ans;    
 }
 
+// Decimal to Binary conversion
+int binarytodecimal(int binum){
+    int ans = 0, pow = 1; // intial ans = 0 and pow = 1 (2^0)
+
+    while (binum > 0)
+    {
+        int rem = binum % 10;    // remainder when divided by 10
+        ans = ans + (rem * pow);
+        
+        binum = binum / 10;      // updating binum when divided by 10
+        pow = pow * 2;           // pow is below because we want to add numbers in 2^0, 2^1, 2^2...  in forward direction
+    }
+    return ans;
+}
+
+
 int main() {
-    int n;
+    // Decimal to Binary conversion
+    int decnum;
     cout << "Enter a decimal number: ";
-    cin >> n;
+    cin >> decnum;
 
-    int binarynum = dectobinary(n);
-    cout << "Binary of " << n << " is " << binarynum << endl;
+    cout << "Binary of " << decnum << " is " << dectobinary(decnum) << endl;
 
-    for (int i = 0; i < 31; i++)
+    for (int i = 11; i < 31; i++)
     {
         cout << dectobinary(i) << endl; // printing binary of first 30 numbers
     }
 
+    // Binary to Decimal conversion
+    int binum;
+    cout << "Enter a binary number: ";
+    cin >> binum;
+
+    cout << "Decimal of " << binum << " is " << binarytodecimal(binum) << endl;
+    
     return 0;
 }
