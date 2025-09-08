4160a9ff3c4c4b696471ec57228a18d292c08a5a
varuncode01 <varun1992.xyz@gmail.com>
2025-09-09 02:45:12 +0530

    arrays pass by reference

--- day6.cpp
+++ day6.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+// pass by reference
+void changeArr(int arr[], int size) {
+    cout << "In Function\n";
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = 2*arr[i];
+    }
+}
+
 int main() {
     int arr1[10] = {2,3,4,5,1,6,7,8,9,10};
     int arr2[] = {11,12,13,14,15,16,17,18,19,20}; // automatic size is according to the number of elements
@@ -22,5 +31,16 @@ int main() {
     }
     cout << "Smallest element: " << smallest << endl;
     cout << "Largest element: " << largest << endl;
+
+    // pass by reference
+    int sizearr2 = sizeof(arr2) / sizeof(int);
+    changeArr(arr2, sizearr2);
+
+    cout << "In Main Function\n";
+    for (int i = 0; i < sizearr2; i++)
+    {
+        cout << arr2[i] << "\n";           // changed values of original array in main function
+    }
+    
     return 0;
 }
