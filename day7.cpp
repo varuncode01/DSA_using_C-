1b890c0f7bd827330f6de246985a36e3216ebffd
varuncode01 <varun1992.xyz@gmail.com>
2025-09-10 05:23:35 +0530

    vector imp functions

--- day7.cpp
+++ day7.cpp
@@ -22,10 +22,25 @@ int main() {
     {
         cout << vec2[i] << " ";
     }
+    /*
     cout << "\nSize of vec3: " << vec3.size() << endl;
-    for (int i : vec3)
+    for (int i : vec3)   // range based for loop no need to do it like this for (int i = 0; i < vec3.size(); i++)
     {
         cout << vec3[i] << " ";
     }
+    */ 
+
+    // vector functions
+    cout << "size of vec1: " << vec1.size() << endl;  // size function
+    cout << "capacity of vec1: " << vec1.capacity() << endl; // capacity function
+    cout << "max size of vec1: " << vec1.max_size() << endl; // max_size function
+    vec3.push_back(25); // push_back function to add element at the end
+    cout << "size of vec3: " << vec3.size() << endl;
+    vec3.pop_back(); // pop_back function to remove element from the end
+    cout << "size of vec3: " << vec3.size() << endl;
+    cout << "Element at index 2: " << vec1.at(2) << endl; // at function to access element at specific index
+    cout << "Front element: " << vec1.front() << endl; // front function
+    cout << "Back element: " << vec1.back() << endl; // back function
     
+    return 0;
 }
