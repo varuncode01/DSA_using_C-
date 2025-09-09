eb37cb0294681070caa2e51b42ee76aed45d8421
varuncode01 <varun1992.xyz@gmail.com>
2025-09-10 03:57:15 +0530

    vector intialization

--- day7.cpp
+++ day7.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+// Vector's size is dynamic unlike arrays which have fixed size
+// Vector is a template class in C++ STL (Standard Template Library)
 int main() {
-    cout << "Day 7" << endl;
+    vector<int> vector_name; // empty vector 
+    cout << "Size of vector: " << vector_name.size() << endl;
+    // cout << vector_name[0] << endl; // error as vector is empty
 }
