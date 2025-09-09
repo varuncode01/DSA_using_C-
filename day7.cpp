f1906a6ee417b401b57b124e978c751b283d1b76
varuncode01 <varun1992.xyz@gmail.com>
2025-09-10 04:05:12 +0530

    vector intialization 1

--- day7.cpp
+++ day7.cpp
@@ -5,7 +5,27 @@ using namespace std;
 // Vector's size is dynamic unlike arrays which have fixed size
 // Vector is a template class in C++ STL (Standard Template Library)
 int main() {
+    // Vector declaration
     vector<int> vector_name; // empty vector 
     cout << "Size of vector: " << vector_name.size() << endl;
     // cout << vector_name[0] << endl; // error as vector is empty
+    vector<int> vec1 = {1,2,3,4,5}; // vector of size 5
+    vector<int> vec2(5); // vector of size 5 with all elements initialized to 0
+    vector<int> vec3(5, 10); // vector of size 5 with all elements initialized to 10
+    cout << "Size of vec1: " << vec1.size() << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << vec1[i] << " ";
+    }
+    cout << "\nSize of vec2: " << vec2.size() << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << vec2[i] << " ";
+    }
+    cout << "\nSize of vec3: " << vec3.size() << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << vec3[i] << " ";
+    }
+    
 }
