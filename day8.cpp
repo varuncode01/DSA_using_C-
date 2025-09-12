834b0e3ddec01ae5fc148f498762e8cc5ad18751
varuncode01 <varun1992.xyz@gmail.com>
2025-09-13 04:54:31 +0530

    creating subarrays

--- day8.cpp
+++ day8.cpp
@@ -2,6 +2,23 @@
 #include <vector>
 using namespace std;
 
+// subarray = n*(n+1)/2, n = size of array
+
 int main(){
+    vector <int> arr = {1,2,3,4,5};
+    // subarray
+    for (int st = 0; st < arr.size(); st++)
+    {
+        for (int end = 0; end < arr.size(); end++)
+        {
+            for (int i = st; i <= end; i++)
+            {
+                cout << arr[i];
+            }
+            cout << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
