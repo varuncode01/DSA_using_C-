62bd9d678c2a6052844e1e497f91973a6347192f
varuncode01 <varun1992.xyz@gmail.com>
2025-09-14 06:12:22 +0530

    mj using brute force

--- day9.cpp
+++ day9.cpp
@@ -51,21 +51,53 @@ vector<int> pairsumopt(vector<int> nums, int target){
     return ans;    
 }
 
+
+// Majority Element
+// Brute Force Approach
+// Time Complexity: O(n^2)
+int majorityElementbf(vector<int> nums){
+    int n = nums.size();
+    int majority = n/2;
+    int ans;
+    for (int val: nums)
+    {
+        int frequency = 0;
+        for (int ele: nums)
+        {
+            if (ele == val){
+                frequency++;
+            }
+        }
+        if (frequency > majority){
+            return val;
+        }
+    }
+    return -1;
+}
+
 int main(){
+    // Pairsum
     vector<int> nums = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
     int target = 21;
-    vector<int> ans = pairsumbf(nums, target); //using brute force
+    
+    //using brute force
+    vector<int> ans = pairsumbf(nums, target);
     for (int i = 0; i < ans.size(); i++)
     {
         cout << ans[i] << " ";
     }
     cout << endl;
+    
     // using 2 pointer menthod
     vector<int> ans2 = pairsumopt(nums, target);
     for (int i = 0; i < ans.size(); i++)
     {
         cout << ans[i] << " ";
     }
+
+    // Majority Element
+    vector<int> nums2 = {2,2,1,1,1,2,2,3,1,4,3,3,4,4,4,4,5,6,7,8,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4};
+    cout << "\nMajority Element is: " << majorityElementbf(nums2) << endl;
 
     return 0;
 }
